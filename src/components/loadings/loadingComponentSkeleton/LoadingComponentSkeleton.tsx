fc250648d4513502ccf293ffc88f7d47b4d68b88
pedrodevsz <pedrodevs25@gmail.com>
2025-09-22 09:42:35 -0300

    feat: (component) create loding skeletons

---
+++ src/components/loadings/loadingComponentSkeleton/LoadingComponentSkeleton.tsx
@@ -0,0 +1,8 @@
+export function LoadingComponentSkeleton() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <div
+                className="w-10 h-10 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin" ></div>
+        </div>
+    )
+}
---
+++ src/components/loadings/loadingPageSkeleton/LoadingPageSkeleton.tsx
@@ -0,0 +1,16 @@
+export function LoadingPageSkeleton() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="flex-col gap-4 w-full flex items-center justify-center">
+                <div
+                    className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+                >
+                    <div
+                        className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
+                    ></div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
